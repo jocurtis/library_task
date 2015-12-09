6ef673342d7f863cad5671bb00e8be7c57e4c9c9
Joseph Curtis <jocurtis@gmail.com>
2015-12-09 23:00:12 +1000

    Small cleanup

--- src/main/java/org/sporc/BookRepository.java
+++ src/main/java/org/sporc/BookRepository.java
@@ -3,7 +3,6 @@ package org.sporc;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 
